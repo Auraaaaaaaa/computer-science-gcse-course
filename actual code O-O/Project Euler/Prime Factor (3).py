8661c376a6fd03cf6365efc3028088f0a783fc79
bigmanhandles <56513089+bigmanhandles@users.noreply.github.com>
2022-03-14 11:54:16 +0000

    prime factors

---
+++ actual code O-O/Project Euler/Prime Factor (3).py
@@ -0,0 +1,23 @@
+
+def isprime(num):
+    flag = False
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = True
+                break
+    if flag:
+        return False
+    else:
+      return True
+
+num=int(input("enter a number"))
+factors=[]
+for i in range(1,num+1):
+    if num%i==0:
+       if num % 1 == 0:
+            if isprime(i):
+                factors.append(i)
+            
+
+print ("Factors of {} = {}".format(num,factors))
