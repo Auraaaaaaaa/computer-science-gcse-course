9428be34a4743c4b4bf03025c4a39a0a5e5176e6
bigmanhandles <56513089+bigmanhandles@users.noreply.github.com>
2022-03-25 14:03:05 +0000

    d

--- actual code O-O/Project Euler/Highly divisible triangle number (12).py
+++ actual code O-O/Project Euler/Highly divisible triangle number (12).py
@@ -1,1 +1,14 @@
-#calculate the largest triangular number with 500 divisors
+num = 1
+amntofdivisors = 0
+def isTriangular(n):
+    for i in range(1,n):
+        if (i*(i+1))/2 == n:
+            return True
+    return False
+
+while amntofdivisors < 500:
+    num = num + 1
+    if isTriangular(num):
+        amntofdivisors = amntofdivisors + 1
+        print(f"amnt of divisors {amntofdivisors}")
+print(num)
