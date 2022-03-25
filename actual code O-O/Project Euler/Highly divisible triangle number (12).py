8a516e9ffb23112066aeaa2d09ddb5d39aa9582c
bigmanhandles <56513089+bigmanhandles@users.noreply.github.com>
2022-03-25 14:23:12 +0000

    cheese

--- actual code O-O/Project Euler/Highly divisible triangle number (12).py
+++ actual code O-O/Project Euler/Highly divisible triangle number (12).py
@@ -1,14 +1,19 @@
-num = 1
-amntofdivisors = 0
-def isTriangular(n):
-    for i in range(1,n):
-        if (i*(i+1))/2 == n:
-            return True
-    return False
+import math
+from time import time
+t = time()
+def divisors(n):
+    number_of_factors = 0
+    for i in range(1, int(math.ceil(math.sqrt(n)))):
+        if n % i == 0:
+            number_of_factors +=2
+        else:
+            continue
+    return number_of_factors
 
-while amntofdivisors < 500:
-    num = num + 1
-    if isTriangular(num):
-        amntofdivisors = amntofdivisors + 1
-        print(f"amnt of divisors {amntofdivisors}")
-print(num)
+x=1
+for y in range(2,1000000):
+    x += y
+    if divisors(x) >= 500:
+        print (x)
+        break
+tt = time()-t
